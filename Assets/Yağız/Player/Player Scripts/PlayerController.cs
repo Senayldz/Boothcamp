184c98bb8da4839ef46f60a5cbf3ff27ad385ff8
yagizt01 <yagizturk00@gmail.com>
2023-06-16 21:34:16 +0300

    basic player movement added

---
+++ Assets/Yağız/Player/Player Scripts/PlayerController.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float playerSpeed;
+    [SerializeField] float jumpForce;
+    [SerializeField] float groundCheckRadius;
+
+    public LayerMask groundLayer;
+
+    public GameObject groundCheck;
+
+    bool facingRight;
+    bool isGrounded;
+
+    Collider[] groundcollision;
+
+    Animator playerAnim;
+
+    Rigidbody playerRb;
+
+    void Start()
+    {
+        playerAnim = GetComponent<Animator>();
+        playerRb = GetComponent<Rigidbody>();
+        facingRight = true;
+
+    }
+
+    void FixedUpdate()
+    {
+        Movement();
+    }
+
+    void Update()
+    {
+        Jump();
+        PreventInfiniteJump();
+    }
+
+
+    void Movement()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        playerAnim.SetFloat("speed", Mathf.Abs(moveX));
+
+        playerRb.velocity = new Vector3(moveX * playerSpeed, playerRb.velocity.y, 0);
+
+        if (moveX > 0 && !facingRight)
+        {
+            Flip();
+        }
+
+        else if (moveX < 0 && facingRight)
+        {
+            Flip();
+        }
+
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.W) && isGrounded)
+        {
+            playerRb.velocity = new Vector3(playerRb.velocity.x, jumpForce, 0);
+            
+        }
+        playerAnim.SetBool("grounded", isGrounded);
+
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+
+        Vector3 playerScale = transform.localScale;
+        playerScale.z *= -1;
+        transform.localScale = playerScale;
+    }
+
+    void PreventInfiniteJump()
+    {
+        groundcollision = Physics.OverlapSphere(groundCheck.transform.position, groundCheckRadius, groundLayer);
+
+        if (groundcollision.Length > 0)
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(groundCheck.transform.position, groundCheckRadius);
+    }
+
+
+}
